e59e132591e1650976fe86f098ffd8d16fa5b036
daniel <danielroyde@gmail.com>
2016-03-28 22:22:12 +0100

    Add ordering to Score Class

--- tables/score.py
+++ tables/score.py
@@ -1,7 +1,21 @@
+from functools import total_ordering
+
+
+@total_ordering
 class Score(object):
     def __init__(self, total=None, lowest_table_score=None, lowest_person_score=None):
-        self._total = total
-        self._lowest_table_score = lowest_table_score
-        self._lowest_person_score = lowest_person_score
+        self.total = total
+        self.lowest_table_score = lowest_table_score
+        self.lowest_person_score = lowest_person_score
 
-    def __cmp__(self, other):
+    def __eq__(self, other):
+        return self.total == other.total and \
+               self.lowest_person_score == other.lowest_person_score and \
+               self.lowest_table_score == other.lowest_table_score
+
+    def __lt__(self, other):
+        return self.lowest_table_score < other.lowest_table_score \
+               or (self.lowest_table_score == other.lowest_table_score and self.total < other.total) \
+               or (self.lowest_table_score == other.lowest_table_score
+                   and self.total == other.total
+                   and self.lowest_person_score < other.lowest_person_score)
